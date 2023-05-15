016d2dec5527163c58b045156c91b6d44902e77d
Anton Kabanov <imcreativchik@gmail.com>
2023-05-15 10:57:08 +0500

    add name, namespace

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-
 from . import views
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:id>/', views.post_detail, name='index'),
-    path('<slug:category_slug>/', views.category_posts, name='index')
+    path('<int:id>/', views.post_detail, name='post_detail'),
+    path('<slug:category_slug>/', views.category_posts, name='category_posts')
 ]
--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'pages'
 
 urlpatterns = [
-
+    path('about/', views.about, name='about'),
+    path('rules/', views.rules, name='rules'),
 ]
