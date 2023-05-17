3c7d4057872ab065a6529bed7c7d77be87cdd377
Anton Kabanov <imcreativchik@gmail.com>
2023-05-17 22:51:40 +0500

    Add docstrings

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 
 
 def about(request):
+    """ Выводит информацию о проекте """
     template_name = 'pages/about.html'
     return render(request, template_name)
 
 
 def rules(request):
+    """ Выводит информацию о правилах на проекте """
     template_name = 'pages/rules.html'
     return render(request, template_name)
 
